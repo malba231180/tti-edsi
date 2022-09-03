73098d01aebc43bd1d7df4571b68d32fb4f56c81
Marcelo Alba <malba@gcgiglobal.com>
2022-09-03 04:19:41 -0400

    Update composer.lock, acorn, and 15027 more files...

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,42 +1,41 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-main',
-    'version' => 'dev-main',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'name' => 'malba231180/tti-edsi',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '34d0f5e3aceb7f05dcef68e56ccb18b2c6ac1e04',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
     ),
-    'reference' => '29f767ad9b5e8766efcecc1f13709992a0b6b418',
-    'name' => 'malba231180/tti-edsi',
-  ),
-  'versions' => 
-  array (
-    'malba231180/tti-edsi' => 
-    array (
-      'pretty_version' => 'dev-main',
-      'version' => 'dev-main',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '29f767ad9b5e8766efcecc1f13709992a0b6b418',
+    'versions' => array(
+        'malba231180/tti-edsi' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '34d0f5e3aceb7f05dcef68e56ccb18b2c6ac1e04',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'squizlabs/php_codesniffer' => array(
+            'pretty_version' => '3.7.1',
+            'version' => '3.7.1.0',
+            'reference' => '1359e176e9307e906dc3d890bcc9603ff6d90619',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../squizlabs/php_codesniffer',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+        'wp-coding-standards/wpcs' => array(
+            'pretty_version' => '1.2.1',
+            'version' => '1.2.1.0',
+            'reference' => 'f328bcafd97377e8e5e5d7b244d5ddbf301a3a5c',
+            'type' => 'phpcodesniffer-standard',
+            'install_path' => __DIR__ . '/../wp-coding-standards/wpcs',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
     ),
-    'squizlabs/php_codesniffer' => 
-    array (
-      'pretty_version' => '3.7.1',
-      'version' => '3.7.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '1359e176e9307e906dc3d890bcc9603ff6d90619',
-    ),
-    'wp-coding-standards/wpcs' => 
-    array (
-      'pretty_version' => '1.2.1',
-      'version' => '1.2.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'f328bcafd97377e8e5e5d7b244d5ddbf301a3a5c',
-    ),
-  ),
 );
